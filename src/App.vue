a4f6a2b7824937640b12540ec19f37cf501259e6
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-11-10 18:37:53 +0900

    add: axios追加

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Start @hotels="hotels = $event"></Start>
+    <HotelSearch :hotels="hotels"></HotelSearch>
   </div>
 </template>
 
 <script>
+import Start from "./components/Start.vue";
+import HotelSearch from "./components/HotelSearch.vue";
 
 export default {
-  
-}
+  data() {
+    return {
+      hotels: {}
+    }
+  },
+  components: {
+    Start,
+    HotelSearch
+  },
+};
 </script>
 
 <style>
-
 </style>
