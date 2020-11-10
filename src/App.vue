5d3cd5398d543528f4b246b654ec28eaee66436d
宮田裕己 <ruby.yukimiyata@gmail.com>
2020-11-10 22:19:37 +0900

    add: ハンバーガーメニュー機能を追加

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <template>
   <div id="app">
-    <Start @hotels="hotels = $event" @restaurants="restaurants = $event"></Start>
-    <RestaurantSearch :restaurants="restaurants"></RestaurantSearch>
-    <HotelSearch :hotels="hotels"></HotelSearch>
+    <Slide :closeOnNavigation="true">
+      <p @click="setComponent('HotelSearch')" class="hamburger-menu">ホテル</p>
+      <p @click="setComponent('RestaurantSearch')" class="hamburger-menu">飲食店</p>
+      <p @click="setComponent('Start')" class="hamburger-menu">再検索</p>
+    </Slide>
+
+    <template v-if="currentComponent == 'Start'">
+      <Start @hotels="hotels = $event" @restaurants="restaurants = $event"></Start>
+    </template>
+    <template v-else-if="currentComponent == 'HotelSearch'">
+      <HotelSearch :hotels="hotels"></HotelSearch>
+    </template>
+    <template v-else-if="currentComponent == 'RestaurantSearch'">
+      <RestaurantSearch :restaurants="restaurants"></RestaurantSearch>
+    </template>
   </div>
 </template>
 
 <script>
+import { Slide } from "vue-burger-menu";
 import Start from "./components/Start.vue";
 import HotelSearch from "./components/HotelSearch.vue";
-import RestaurantSearch from "./components/RestaurantSearch.vue"
+import RestaurantSearch from "./components/RestaurantSearch.vue";
 
 export default {
   data() {
     return {
       hotels: {},
-      restaurants: {}
-    }
+      restaurants: {},
+      currentComponent: "Start"
+    };
   },
   components: {
+    Slide,
     Start,
     HotelSearch,
     RestaurantSearch
   },
+  methods: {
+    setComponent(componentName) {
+      this.currentComponent = componentName;
+    }
+  }
 };
 </script>
 
 <style>
+.hamburger-menu {
+  cursor: pointer;
+}
 </style>
